b917cecf2fbe263b41cf636cbe44c5173f38bada
Sanjana A <sanjanaa589@gmail.com>
2026-07-13 21:51:55 +0530

    Completed 5-Agent LangGraph workflow

--- graph/workflow.py
+++ graph/workflow.py
@@ -3,6 +3,8 @@ from memory.state import AgentState
 from agents.planner import planner_agent
 from agents.researcher import research_agent
 from agents.coder import coding_agent
+from agents.reviewer import reviewer_agent
+from agents.reporter import reporter_agent
 
 # Create the graph
 workflow = StateGraph(AgentState)
@@ -15,11 +17,18 @@ workflow.add_node("research", research_agent)
 
 # Add Coding node
 workflow.add_node("coder", coding_agent)
+# Add Reviewer node
+workflow.add_node("reviewer", reviewer_agent)
+
+# Add Reporter node
+workflow.add_node("reporter", reporter_agent)
 
 workflow.add_edge(START, "planner")
 workflow.add_edge("planner", "research")
 workflow.add_edge("research", "coder")
-workflow.add_edge("coder", END)
+workflow.add_edge("coder", "reviewer")
+workflow.add_edge("reviewer", "reporter")
+workflow.add_edge("reporter", END)
 
 # Compile the graph
 graph = workflow.compile()
@@ -46,3 +55,9 @@ print(result["research"])
 
 print("\n===== Coding Output =====")
 print(result["code"])
+
+print("\n===== Reviewer Output =====")
+print(result["review"])
+
+print("\n===== Final Report =====")
+print(result["report"])
